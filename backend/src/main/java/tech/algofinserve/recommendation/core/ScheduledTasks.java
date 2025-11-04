6a5121d6b32051f21c32d8fae1cf72d5f3c14001
snehal shah <snehal.shah143@gmail.com>
2025-11-04 07:53:46 +0530

    changed the cron timing

--- backend/src/main/java/tech/algofinserve/recommendation/core/ScheduledTasks.java
+++ backend/src/main/java/tech/algofinserve/recommendation/core/ScheduledTasks.java
@@ -10,7 +10,7 @@ public class ScheduledTasks {
 
   @Autowired private ChartInkAlertProcessingService alertProcessing;
 
-  @Scheduled(cron = "0 30 19 * * ?") // Cron expression for running every minute
+  @Scheduled(cron = "0 59 15 * * ?") // Cron expression for running every minute
   public void execute() {
     alertProcessing.generateStockAlertOutputReport();
     System.out.println("Scheduler ran for report genration::" + new Date());
