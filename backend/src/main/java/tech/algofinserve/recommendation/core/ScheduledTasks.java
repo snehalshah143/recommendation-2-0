6d442d97f61207e7f6233d5f8f328b22b8f17649
snehal shah <snehal.shah143@gmail.com>
2025-11-13 22:52:27 +0530

    crontesting

--- backend/src/main/java/tech/algofinserve/recommendation/core/ScheduledTasks.java
+++ backend/src/main/java/tech/algofinserve/recommendation/core/ScheduledTasks.java
@@ -1,12 +1,16 @@
 package tech.algofinserve.recommendation.core;
 
 import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledTasks {
+  private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 
   @Autowired private ChartInkAlertProcessingService alertProcessing;
 
@@ -14,6 +18,7 @@ public class ScheduledTasks {
   @Scheduled(cron = "0 00 23 * * ?") // Cron expression for running every minute
   public void execute() {
     alertProcessing.generateStockAlertOutputReport();
+    logger.info("Scheduler ran for report genration::" + new Date());
     System.out.println("Scheduler ran for report genration::" + new Date());
   }
 }
