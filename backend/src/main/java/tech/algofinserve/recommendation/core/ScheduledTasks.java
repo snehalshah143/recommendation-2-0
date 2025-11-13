d19783e57ee8a953acdc7bc4b229ace184d0280d
snehal shah <snehal.shah143@gmail.com>
2025-11-13 23:10:14 +0530

    crontesting

--- backend/src/main/java/tech/algofinserve/recommendation/core/ScheduledTasks.java
+++ backend/src/main/java/tech/algofinserve/recommendation/core/ScheduledTasks.java
@@ -1,5 +1,6 @@
 package tech.algofinserve.recommendation.core;
 
+import java.time.ZoneId;
 import java.util.Date;
 
 import org.slf4j.Logger;
@@ -8,13 +9,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class ScheduledTasks {
   private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 
   @Autowired private ChartInkAlertProcessingService alertProcessing;
-
-//  @Scheduled(cron = "0 59 15 * * ?") // Cron expression for running every minute
+  @PostConstruct
+  public void init() {
+    logger.info("✅ Scheduler bean loaded successfully");
+    logger.info("Container time now: " + new Date());
+    logger.info("System default timezone: " + ZoneId.systemDefault());
+  }
+//  @Scheduled(cron = "0 58 15 * * ?") // Cron expression for running every minute
 //  @Scheduled(cron = "0 00 23 * * ?") // Cron expression for running every minute
   @Scheduled(cron = "0 05 23 * * ?", zone = "Asia/Kolkata")
   public void execute() {
